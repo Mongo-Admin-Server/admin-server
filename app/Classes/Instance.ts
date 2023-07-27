e0ab9520f27fbb36e5e936447327509a7636b6f1
rouan Laporal <rouan.laporal@gmail.com>
2023-07-27 09:16:02 +0200

    feat : connection return a Mongoclient

--- app/Classes/Instance.ts
+++ app/Classes/Instance.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export class Instance{
     
-    public async connection() :  Promise<boolean | unknown>{
+    public async connection() :  Promise<MongoClient | boolean>{
         if(!process.env.MONGODB_URI)
             return false;
         const uri = process.env.MONGODB_URI;
@@ -12,11 +12,9 @@ export class Instance{
             let pingResponse = await client.db('admin').command({ ping:1 });
             if(!pingResponse.ok)
                 return false;
-            return true;
+            return client;
         }catch(error){
             throw(error);
-        }finally{
-            await client.close();
         }
     }
 }
