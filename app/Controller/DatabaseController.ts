6b4d70ec37884fa45ad773864eb8e30365959d94
rouan.laporal <rouan.laporal@gmail.com>
2023-08-03 18:44:44 +0200

    refacto: add the number of collection to the database route response

--- app/Controller/DatabaseController.ts
+++ app/Controller/DatabaseController.ts
@@ -3,10 +3,7 @@ import { NextApiResponse } from "next";
 
 export class DatabaseController{
     public async getDatabases(response: NextApiResponse){
-        let databases = await new Database().listDatabase();
-        response.status(200).json({
-            status: 'success',
-            data: databases.databases
-        });
+        const databases = await new Database().listDatabase();
+        response.status(200).json(databases)
     }
 }
