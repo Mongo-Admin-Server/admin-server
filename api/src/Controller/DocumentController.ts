6d7fcc49cfd34b9297b84c3433b235221b61fec2
FeretJocelin <feret.jocelin@gekko-holding.com>
2023-09-06 14:33:03 +0200

    add routes to get one create and delete a document

--- api/src/Controller/DocumentController.ts
+++ api/src/Controller/DocumentController.ts
@@ -5,9 +5,9 @@ export class DocumentController {
     
     public async getAllDocumentsByCollection(response: NextApiResponse, databaseName: string | string[], collectionName: string | string[]): Promise<any> {
         const documents = await new Documents().getAllDocumentsByCollection(databaseName, collectionName);
-        const countDocuments = await new Documents().countDocumentsByCollection(collectionName);
-        const avgSizeDocument = await new Documents().averageSizeDocumentsByCollection(collectionName);
-        const totalSizeDocument = await new Documents().totalSizeDocumentsByCollection(collectionName);
+        const countDocuments = await new Documents().countDocumentsByCollection(databaseName, collectionName);
+        const avgSizeDocument = await new Documents().averageSizeDocumentsByCollection(databaseName, collectionName);
+        const totalSizeDocument = await new Documents().totalSizeDocumentsByCollection(databaseName, collectionName);
         if(!documents ) {
             response.status(404).json('error');
         } else {
@@ -21,6 +21,45 @@ export class DocumentController {
                     totalSize: totalSizeDocument
                 });
             } catch(error) {
+                response.status(500).json('error');
+            }
+        }
+    }
+
+    public async getOneDocument(response: NextApiResponse, databaseName: string | string[], collectionName: string | string[], id: string | string[]): Promise<any> {
+        const document = await new Documents().getOneDocument(databaseName, collectionName, id);
+        if(!document) {
+            response.status(404).json('error');
+        } else {
+            try {
+                response.status(200).json(document);
+            } catch(error) {
+                response.status(500).json('error');
+            }
+        }
+    }
+
+    public async addOneDocument(response: NextApiResponse, databaseName: string | string[], collectionName: string | string[], query: JSON): Promise<any> {
+        const newDocument = await new Documents().addOneDocument(databaseName, collectionName, query);
+        if(!newDocument) {
+            response.status(404).json('Error, no document provided for the inserting');
+        } else {
+            try {
+                response.status(200).json(newDocument);
+            } catch(error) {
+                response.status(500).json('error');
+            }
+        }
+    }
+
+    public async DeleteOneDocument(response: NextApiResponse, databaseName: string | string[], collectionName: string | string[], id: string | string[]): Promise<any> {
+        const deleteDocument = await new Documents().DeleteOneDocument(databaseName, collectionName, id);
+        if(!deleteDocument) {
+            response.status(404).json('Error, no document provided for the deleting');
+        } else {
+            try {
+                response.status(200).json(deleteDocument);
+            } catch (error) {
                 response.status(500).json('error');
             }
         }
