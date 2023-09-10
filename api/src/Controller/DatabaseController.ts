55d9ab2038a633b0dadbb36f39bc523a60544902
Louis Poupat <louispoupat@sfr.fr>
2023-09-10 21:28:44 +0200

    revert

--- api/src/Controller/DatabaseController.ts
+++ api/src/Controller/DatabaseController.ts
@@ -22,7 +22,8 @@ export class DatabaseController{
 
     public async deleteDatabase(request: NextApiRequest, response: NextApiResponse){
         const { databaseName } = request.query;
-        if(!databaseName) response.send(new ApiError(400, 'query/not-found', 'database_name_not_found'));
+        if(!databaseName) 
+            response.send(new ApiError(400, 'query/not-found', 'database_name_not_found'));
         const status = await new Database().dropDatabase(databaseName);
         response.status(200).json(status);
     }
