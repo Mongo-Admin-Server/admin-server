385059bceb8a5f80c3be05a5d33b54ccf9d8e3d4
rouan.laporal <rouan.laporal@gmail.com>
2023-09-11 11:54:14 +0200

    fix: prevent to send a status 200 when an error appear

--- api/src/Controller/DatabaseController.ts
+++ api/src/Controller/DatabaseController.ts
@@ -22,9 +22,12 @@ export class DatabaseController{
 
     public async deleteDatabase(request: NextApiRequest, response: NextApiResponse){
         const { databaseName } = request.query
-        if(!databaseName)
-            response.send(new ApiError(400, 'query/not-found', 'database_name_not_found'));
         const status = await new Database().dropDatabase(databaseName);
-        response.status(200).json(status);
+        if(status === true)
+            response.status(200).json(status);
+        else if(status === false)
+            response.status(500);
+        else
+            response.status(status.code).json(status.json)
     }
 }
