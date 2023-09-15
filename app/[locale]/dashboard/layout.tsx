f4401d0934ac06bacf116f21530f496d2281298e
Louis Poupat <louispoupat@sfr.fr>
2023-09-15 15:59:13 +0200

    fix

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,9 +1,6 @@
 'use client';
 import { useEffect } from 'react';
 
-import { I18nProviderClient } from '@/shared/locales/clients';
-import fr from '@/shared/locales/fr';
-
 import styles from './dashboard.module.scss';
 
 import SideMenu from '@components/menu/SideMenu';
@@ -13,10 +10,10 @@ import { fetchAllDatabase } from '@/domain/usecases/database-slice';
 
 export default function DashboardLayout({
   children,
-  params: { locale }
+  params: { locale },
 }: {
   children: React.ReactNode;
-  params: { locale: string }
+  params: { locale: string };
 }) {
   const dispatch = useDispatch();
 
@@ -25,11 +22,9 @@ export default function DashboardLayout({
   }, [dispatch]);
 
   return (
-    <I18nProviderClient locale={locale} fallback={<p> Loading...</p>} fallbackLocale={fr}>
-      <main className={styles.main}>
-        <SideMenu />
-          <section className={styles.container}>{children}</section>
-      </main>
-    </I18nProviderClient>
+    <main className={styles.main}>
+      <SideMenu />
+      <section className={styles.container}>{children}</section>
+    </main>
   );
 }
